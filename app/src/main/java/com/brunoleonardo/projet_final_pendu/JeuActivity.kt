038f87df616772f81438da7ad6f5191a393825e1
Bruno-1333 <2295132@cegepoutaouais.qc.ca>
2023-07-21 20:34:03 -0400

    version 4. functionalle

--- app/src/main/java/com/brunoleonardo/projet_final_pendu/JeuActivity.kt
+++ app/src/main/java/com/brunoleonardo/projet_final_pendu/JeuActivity.kt
@@ -2,10 +2,18 @@ package com.brunoleonardo.projet_final_pendu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.brunoleonardo.projet_final_pendu.databinding.ActivityJeuBinding
 
 class JeuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityJeuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_jeu)
+        binding = ActivityJeuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Récupération des données de l'activité précédente (PanneauJeuActivity)
+        val theme = intent.getStringExtra("theme")
+        val difficulty = intent.getStringExtra("difficulte")
+
     }
 }
--- app/src/main/java/com/brunoleonardo/projet_final_pendu/PanneauJeuActivity.kt
+++ app/src/main/java/com/brunoleonardo/projet_final_pendu/PanneauJeuActivity.kt
@@ -3,11 +3,16 @@ package com.brunoleonardo.projet_final_pendu
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import android.widget.Toast
 import com.brunoleonardo.projet_final_pendu.databinding.ActivityPanneauJeuBinding
 
 class PanneauJeuActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPanneauJeuBinding
+
+    // Referência ao botão de tema atualmente selecionado e suas imagens correspondentes
+    private var currentThemeButton: Pair<ImageButton, Int>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,30 +22,37 @@ class PanneauJeuActivity : AppCompatActivity() {
         var themeChoisi = ""
         var difficulteChoisi = ""
 
-        binding.imgBtnAnimaux.setOnClickListener {
-            themeChoisi = "Animal"
-            // Você pode querer alterar a aparência do ImageButton para indicar que ele foi selecionado
-        }
+        val themeButtons = mapOf(
+            binding.imgBtnAnimaux to Triple("Animal", R.drawable.img_animal_normal, R.drawable.img_animal_choisi),
+            binding.imgBtnPays to Triple("Pays", R.drawable.img_pays_normal, R.drawable.img_pays_choisi),
+            binding.imgBtnInstumentsMusique to Triple("Musique", R.drawable.img_musique_normal, R.drawable.img_musique_choisi),
+            binding.imgBtnVoitures to Triple("Voitures", R.drawable.img_voitures_normal, R.drawable.img_voitures_choisi)
+        )
 
-        binding.imgBtnPays.setOnClickListener {
-            themeChoisi = "Pays"
-            // Alterar a aparência do ImageButton, se necessário
-        }
+        for ((button, info) in themeButtons) {
+            val (theme, imgNormal, imgChoisi) = info
 
-        binding.imgBtnInstumentsMusique.setOnClickListener {
-            themeChoisi = "Musique"
-            // Alterar a aparência do ImageButton, se necessário
-        }
+            button.setImageResource(imgNormal) // Definir a imagem inicial do botão
 
-        binding.imgBtnVoitures.setOnClickListener {
-            themeChoisi = "Voitures"
-            // Alterar a aparência do ImageButton, se necessário
-        }
+            button.setOnClickListener {
+                // Desmarque o botão de tema atualmente selecionado, se houver um
+                currentThemeButton?.let { (currentButton, currentImgNormal) ->
+                    currentButton.setImageResource(currentImgNormal)
+                }
 
-        // Faça o mesmo para os outros botões de tema
+                // Marque o novo botão como selecionado
+                button.setImageResource(imgChoisi)
+
+                // Atualize a referência ao botão de tema atualmente selecionado e sua imagem normal
+                currentThemeButton = button to imgNormal
+
+                // Atualize o tema escolhido
+                themeChoisi = theme
+
+            }
+        }
 
         binding.groupeRadioDifficulte.setOnCheckedChangeListener { group, checkedId ->
-            // Fait la même chose pour les niveaux de difficulté
             difficulteChoisi = when (checkedId) {
                 R.id.radioBtnFacille -> "Facile"
                 R.id.radioBtnMoyen -> "Moyen"
@@ -50,17 +62,15 @@ class PanneauJeuActivity : AppCompatActivity() {
         }
 
         binding.btnJouer.setOnClickListener {
-            // Vérifiez si un thème et un niveau de difficulté ont été sélectionnés
             if (themeChoisi.isNotEmpty() && difficulteChoisi.isNotEmpty()) {
-                // Si oui, commencez JeuActivity avec un Intent, en passant les détails de la sélection comme extras
                 val intent = Intent(this, JeuActivity::class.java).apply {
                     putExtra("theme", themeChoisi)
-                    putExtra("difficulty", difficulteChoisi)
+                    putExtra("difficulte", difficulteChoisi)
                 }
                 startActivity(intent)
             } else {
                 Toast.makeText(this, "Veuillez sélectionner un thème et un niveau de difficulté.", Toast.LENGTH_SHORT).show()
             }
         }
-}
+    }
 }
